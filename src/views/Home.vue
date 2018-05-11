5d0720e5fd5168c920d29f240b76203352cdc4ff
Fitrul Islam <fitrul.islam@gmail.com>
2018-05-11 13:23:24 +0700

    add signup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,58 @@
 <template>
 <div class="home">
-  Testing
+  <div class="container">
+    <div v-for="(photo,index) in photos" :key="index">
+      <p>{{photo.user}}</p>
+      <img :src="photo.image" alt="image">
+      <p>{{photo.like.length}}</p>
+      <p>{{photo.caption}}</p>
+      <div v-for="(comment,index) in photo.comment" :key="index">
+        <p>{{comment}}</p>
+      </div>
+      <input type="text" placeholder="comment" v-model="comment">
+      <button type="submit" name="button" @click="addComment">add comment</button>
+    </div>
+  </div>
 </div>
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
-  name: 'home'
+  name: 'home',
+  data () {
+    return {
+      photos: [
+        {
+          user: 'asdasd',
+          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Emperor-cold_hg.jpg/216px-Emperor-cold_hg.jpg',
+          caption: 'apasih kambing',
+          comment: ['kamu tu ya', 'apasih', 'kampret'],
+          like: [123, 123, 123]
+        }
+      ],
+      comment: ''
+    }
+  },
+  methods: {
+    addComment () {
+      axios.post('http://localhost:3000/users/add-comment', {
+
+      })
+        .then(          )
+    }
+  },
+  created: function () {
+    axios.get('http://localhost:3000/users/get-photo')
+      .then(response => {
+        // this.photos = response.data.photo
+        console.log(response)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
 }
 </script>
 
